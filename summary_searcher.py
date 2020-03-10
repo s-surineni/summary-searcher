00b946cc76f445dcb27c1881d23ce374fdda94e0
sampath surineni <sampath.skt@gmail.com>
2020-03-10 20:45:15 +0530

    Wrote the function to get word indices in a summary. Added logging

--- summary_searcher.py
+++ summary_searcher.py
@@ -1,13 +1,24 @@
+from collections import defaultdict
 import json
 import logging
 import os
 import re
 
+logging.basicConfig(format='%(filename)s - %(lineno)d - %(levelname)s - %(message)s',
+                    level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
 def split_summary(summary):
+    # TODO: Find out a way to remove empty string at last
     return re.split(r'[ \W]+', summary)
+
+
+def get_word_indices(summary_words):
+    word_indices = defaultdict(list)
+    for idx, a_word in enumerate(summary_words):
+        word_indices[a_word].append(idx)
+    return word_indices
 
 
 def prepare_inverted_index():
@@ -24,11 +35,15 @@ def prepare_inverted_index():
                 summary_text_split = split_summary(summary_text)
                 title_terms[a_summary['id']] = summary_text_split
 
+            logger.info('Done splitting summaries into words')
+            for a_title_id, summary_words in title_terms.items():
+                logger.info('Getting word indices for book {}'.format(a_title_id))
+                title_terms[a_title_id] = get_word_indices(summary_words)
+
     except FileNotFoundError:
-        logger.error('Make sure youve included file in the folder: {}'
+        logger.error('Make sure you\'ve included file in the folder: {}'
                      .format(os.path.dirname(os.path.realpath(__file__))    
 ))
-
 
 
 prepare_inverted_index()
--- test_summary_searcher.py
+++ test_summary_searcher.py
@@ -1,5 +1,10 @@
-from summary_searcher import split_summary
+from summary_searcher import split_summary, get_word_indices
 
 
 def test_split_summary():
     assert split_summary('a.b.c') == ['a', 'b', 'c']
+
+
+def test_get_word_indices():
+    assert get_word_indices(['the', 'thin', 'the']) == {'the': [0, 2],
+                                                        'thin': [1]}
