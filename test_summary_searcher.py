971b65c8303bb12b7531279765dd1e342149ebb6
sampath surineni <sampath.skt@gmail.com>
2020-03-14 10:51:23 +0530

    1. Done implementing search for phrases 2. Considering number of matches required too 3. Installed coverage.py to check coverage

--- test_summary_searcher.py
+++ test_summary_searcher.py
@@ -1,8 +1,8 @@
-from summary_searcher import split_summary, get_word_indices, get_inverted_index
+from summary_searcher import sanitize_text, get_word_indices, search_summaries
 
 
 def test_split_summary():
-    assert split_summary('a.b.c') == ['a', 'b', 'c']
+    assert sanitize_text('a.b.c') == ['a', 'b', 'c']
 
 
 def test_get_word_indices():
@@ -10,14 +10,15 @@ def test_get_word_indices():
                                                         'thin': [1]}
 
 
-def test_get_inverted_index():
-    fun_input = {'first': {'the': [0, 2],
-                           'thin': [1]},
-                 'second': {'the': [0, 2],
-                            'dime': [1]}}
-    fun_output = {'the': {'first': [0, 2],
-                          'second': [0, 2]},
-                  'thin': {'first': [1]},
-                  'dime': {'second': [1]
-                  }}
-    assert get_inverted_index(fun_input) == fun_output
+def test_search_summaries():
+    result = search_summaries('will make you at', 'data2.json')
+    expected = [{'Anything You Want': {'count': 4, 'words': ['make', 'you', 'at', 'will']}}, {'The Richest Man in Babylon': {'count': 1, 'words': ['you']}}]
+    assert len(result) == len(expected)
+    # Because I don't want to put restriction on order of the words matched
+    for expected_match, result_match in zip(expected, result):
+        assert expected_match.keys() == result_match.keys()
+        summary_name = list(expected_match.keys())[0]
+        assert (expected_match[summary_name]['count'] ==
+                expected_match[summary_name]['count'])
+        assert (set(expected_match[summary_name]['words']) ==
+                set(expected_match[summary_name]['words']))
